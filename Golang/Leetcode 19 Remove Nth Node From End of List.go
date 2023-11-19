2f58c597d487fb65cd1e9a741f448aeb27d8ee8e
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-11-19 23:04:06 +0530

    Update Leetcode 19 Remove Nth Node From End of List.go

--- Golang/Leetcode 19 Remove Nth Node From End of List.go
+++ Golang/Leetcode 19 Remove Nth Node From End of List.go
@@ -23,3 +23,25 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
     Lptr.Next = Lptr.Next.Next
     return dummyHead.Next
 }
+
+//Method 2
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    if head.Next == nil {
+        head = nil
+        return head
+    }
+    Lptr := head
+    Rptr := head
+    for i:=0 ; i < n; i++ {
+        Rptr = Rptr.Next
+    }
+    if Rptr == nil {
+        return head.Next
+    }
+    for Rptr.Next != nil {
+        Rptr = Rptr.Next
+        Lptr = Lptr.Next
+    }
+    Lptr.Next = Lptr.Next.Next
+    return head
+}
