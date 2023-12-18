8d197f2c9709bc6706e0997746fc7c7555a95865
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-12-18 23:37:59 +0530

    Update Leetcode 645 Set Mismatch.go

--- Golang/Leetcode 645 Set Mismatch.go
+++ Golang/Leetcode 645 Set Mismatch.go
@@ -1,21 +1,13 @@
 func findErrorNums(nums []int) []int {
-    newNums := []int{}
-    sum := 0
+    sum, duplicate := 0, 0
     n := len(nums)
     sort.Ints(nums)
-    for i:=0; i<len(nums)-1; i++ {
+    for i:=0; i<n-1; i++ {
         if nums[i] == nums[i+1] {
-            newNums = append(newNums, nums[i])
+            duplicate = nums[i]
         }
         sum += nums[i]
     }
     sum += nums[n-1]
-    // for i:=len(nums)-1; i>0; i-- {
-    //     if (i+1) != nums[i] {
-    //         newNums = append(newNums, (i+1))
-    //         break
-    //     }
-    // }
-    newNums = append(newNums, ((n*(n+1)/2)-sum+newNums[0]) )
-    return newNums
+    return []int{duplicate, ((n*(n+1)/2)-sum+duplicate) }
 }
