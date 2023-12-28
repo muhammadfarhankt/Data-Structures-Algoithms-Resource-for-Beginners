d23755018b444505a51d4619ae8cf8429551f0fb
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2023-12-28 13:43:57 +0530

    Update Leetcode 2176 Count Equal and Divisible Pairs in an Array.go

--- Golang/Leetcode 2176 Count Equal and Divisible Pairs in an Array.go
+++ Golang/Leetcode 2176 Count Equal and Divisible Pairs in an Array.go
@@ -1,7 +1,11 @@
-count := 0
+func countPairs(nums []int, k int) int {
+    count := 0
     for i:=0; i<len(nums)-1; i++ {
         for j:=i+1; j<len(nums); j++ {
             if ( nums[i] == nums[j] ) &&  ( (i * j) % k == 0 ) {
                 count++
             }
         }
+    }
+    return count
+}
