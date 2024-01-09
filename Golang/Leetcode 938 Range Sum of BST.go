722029cdd8b87aafaebb915f790f82d93ab3c643
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-01-09 11:13:04 +0530

    Create Leetcode 938 Range Sum of BST.go

---
+++ Golang/Leetcode 938 Range Sum of BST.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+    if root == nil {
+        return 0
+    }
+    if root.Val >= low && root.Val <= high {
+        return root.Val + rangeSumBST(root.Left,low,high) + rangeSumBST(root.Right,low,high)
+    } else if root.Val < low {
+        return rangeSumBST(root.Right,low,high)
+    } else {
+        return rangeSumBST(root.Left,low,high)
+    }
+    return 0
+}
