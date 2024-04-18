7a3c4271e91cf1118b38b8ee2f7ce26484413c60
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-18 09:37:40 +0530

    Time: 91 ms (95.12%) | Memory: 20.2 MB (7.32%) - LeetSync

--- Golang/3373-maximum-prime-difference/maximum-prime-difference.go
+++ Golang/3373-maximum-prime-difference/maximum-prime-difference.go
@@ -2,14 +2,14 @@ func maximumPrimeDifference(nums []int) int {
     left, right := -1, -1
     for i := 0; i < len(nums); i++ {
         if checkPrime(nums[i]) {
-            fmt.Println(nums[i])
+            //fmt.Println(nums[i])
             left = i
             break
         }
     }
     for i := len(nums)-1; i >= 0; i-- {
         if checkPrime(nums[i]) {
-            fmt.Println(nums[i])
+            //fmt.Println(nums[i])
             right = i
             break
         }
@@ -23,7 +23,7 @@ func checkPrime(num int) bool {
         return true
     }
     prime := true
-    for i := 2; i <= num / 2; i++ {
+    for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
         if num % i == 0 {
             prime = false
             break
