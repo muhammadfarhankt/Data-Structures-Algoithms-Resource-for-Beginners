7b56499f33cac1770c1e6f44703f8fa233b05f17
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-18 10:00:32 +0530

    Time: 98 ms (78.05%) | Memory: 14 MB (92.68%) - LeetSync

--- Golang/3373-maximum-prime-difference/maximum-prime-difference.go
+++ Golang/3373-maximum-prime-difference/maximum-prime-difference.go
@@ -6,22 +6,18 @@ func maximumPrimeDifference(nums []int) int {
 			break
 		}
 	}
-
 	if left == -1 {
 		return 0
 	}
-
 	for i := len(nums) - 1; i >= 0; i-- {
 		if isPrime(nums[i]) {
 			right = i
 			break
 		}
 	}
-
 	if right <= left {
 		return 0
 	}
-
 	return right - left
 }
 
@@ -29,12 +25,10 @@ func isPrime(val int) bool {
 	if val < 2 {
 		return false
 	}
-
 	for i := 2; i <= int(math.Sqrt(float64(val))); i++ {
 		if val%i == 0 {
 			return false
 		}
 	}
-
 	return true
 }
