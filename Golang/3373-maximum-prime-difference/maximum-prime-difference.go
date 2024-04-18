5bb655e4494de6d1230d3407a660093f2f2627c1
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-18 10:00:09 +0530

    Time: 109 ms (52.85%) | Memory: 14.4 MB (80.49%) - LeetSync

--- Golang/3373-maximum-prime-difference/maximum-prime-difference.go
+++ Golang/3373-maximum-prime-difference/maximum-prime-difference.go
@@ -1,29 +1,40 @@
 func maximumPrimeDifference(nums []int) int {
-    left, right := -1, -1
-    for i := 0; i < len(nums); i++ {
-        if checkPrime(nums[i]) {
-            //fmt.Println(nums[i])
-            left = i
-            break
-        }
-    }
-    for i := len(nums)-1; i >= 0; i-- {
-        if checkPrime(nums[i]) {
-            //fmt.Println(nums[i])
-            right = i
-            break
-        }
-    }
-    return right - left
+	left, right := -1, -1
+	for i, v := range nums {
+		if isPrime(v) {
+			left = i
+			break
+		}
+	}
+
+	if left == -1 {
+		return 0
+	}
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if isPrime(nums[i]) {
+			right = i
+			break
+		}
+	}
+
+	if right <= left {
+		return 0
+	}
+
+	return right - left
 }
-func checkPrime(num int) bool {
-    if num == 1 {
-        return false
-    }
-    for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
-        if num % i == 0 {
-            return false
-        }
-    }
-    return true
+
+func isPrime(val int) bool {
+	if val < 2 {
+		return false
+	}
+
+	for i := 2; i <= int(math.Sqrt(float64(val))); i++ {
+		if val%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
