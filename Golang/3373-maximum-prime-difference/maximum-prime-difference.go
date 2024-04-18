573f603fcca36a0a4bb3b5b85cf7437f3a72404a
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-18 10:12:04 +0530

    Time: 101 ms (72.36%) | Memory: 18.1 MB (10.57%) - LeetSync

--- Golang/3373-maximum-prime-difference/maximum-prime-difference.go
+++ Golang/3373-maximum-prime-difference/maximum-prime-difference.go
@@ -1,20 +1,30 @@
-func maximumPrimeDifference(nums []int) int {
-    primesArr := []int{}
-    for i, n := range nums {
-        if checkPrime(n) {
-            primesArr = append(primesArr, i)
-        }
-    }
-    return primesArr[len(primesArr)-1] - primesArr[0]
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
-func checkPrime(num int) bool {
-    if num == 1 {
-        return false
-    }
-    for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
-        if num % i == 0 {
-            return false
-        }
-    }
-    return true
+
+func maximumPrimeDifference(nums []int) int {
+	left := 0
+	right := len(nums) - 1
+	for left < right {
+		var isLeftPrime = isPrime(nums[left])
+		var isRightPrime = isPrime(nums[right])
+		if isLeftPrime && isRightPrime {
+			return right - left
+		}
+		if !isLeftPrime {
+			left++
+		}
+		if !isRightPrime {
+			right--
+		}
+	}
+	return 0
 }
