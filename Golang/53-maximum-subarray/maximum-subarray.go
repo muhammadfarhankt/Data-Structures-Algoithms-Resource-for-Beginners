6ba5b9b4484522b035e1ba9eae9618597f654961
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-19 17:41:13 +0530

    Time: 67 ms (93.43%) | Memory: 7.6 MB (97.54%) - LeetSync

--- Golang/53-maximum-subarray/maximum-subarray.go
+++ Golang/53-maximum-subarray/maximum-subarray.go
@@ -1,15 +1,16 @@
 func maxSubArray(nums []int) int {
-	currMax, globalMax := nums[0], nums[0]
-
-	for i := 1; i < len(nums); i++ {
-        if currMax < 0 {
-			currMax = 0
-		}
-		currMax += nums[i]
-		if globalMax < currMax {
-			globalMax = currMax
-		}
+    if len(nums) == 1 {
+		return nums[0]
 	}
-    
-	return globalMax
+    maxCurr, maxGlobal := nums[0], nums[0]
+    for i := 1; i < len(nums); i++ {
+        if maxCurr < 0 {
+            maxCurr = 0
+        }
+        maxCurr += nums[i]
+        if maxCurr > maxGlobal {
+            maxGlobal = maxCurr
+        }
+    }
+    return maxGlobal
 }
