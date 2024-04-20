f9fe44ababb4701d9016c41b6b81fa27d18553e2
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-20 21:04:12 +0530

    Time: 96 ms (97.22%) | Memory: 15.7 MB (41.67%) - LeetSync

---
+++ Golang/2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.go
@@ -0,0 +1,18 @@
+func countDistinctIntegers(nums []int) int {
+    numsMap := make(map[int]bool)
+    for _, num := range nums {
+        numsMap[num] = true
+        rev := reverse(num)
+        numsMap[rev] = true
+        nums = append(nums, rev)
+    }
+    return len(numsMap)
+}
+func reverse(num int) int {
+    rev := 0
+    for num > 0 {
+        rev = (rev * 10) + (num % 10)
+        num /= 10
+    }
+    return rev
+}
