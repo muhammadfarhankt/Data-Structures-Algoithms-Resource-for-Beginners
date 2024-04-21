f8b39b9dfda98613b9922aa40787b9d64a931321
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-21 11:19:09 +0530

    Time: 26 ms (100.00%) | Memory: 7.1 MB (100.00%) - LeetSync

---
+++ Golang/3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.go
@@ -0,0 +1,21 @@
+func numberOfSpecialChars(word string) int {
+    small, capital := [26]int{}, [26]int{}
+    count := 0
+    for i, char := range word {
+        if char >= 'a' && char <= 'z' {
+            small[int(char - 'a')] = i+1
+        } else if char >= 'A' && char <= 'Z' {
+            if capital[int(char - 'A')] == 0 {
+                capital[int(char - 'A')] = i+1
+            }
+        }
+    }
+    for i := 0; i < 26; i++ {
+        if small[i] != 0 && capital[i] > small[i] {
+            count++
+        }
+    }
+    fmt.Println(small)
+    fmt.Println(capital)
+    return count
+}
