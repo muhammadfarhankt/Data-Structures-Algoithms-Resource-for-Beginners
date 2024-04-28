3a7f029b5bd4b117aff5dc44f5b6c1174d686b8a
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-04-29 04:59:50 +0530

    Time: 2 ms (53.57%) | Memory: 2.1 MB (35.71%) - LeetSync

---
+++ Golang/507-perfect-number/perfect-number.go
@@ -0,0 +1,16 @@
+func checkPerfectNumber(num int) bool {
+    if num == 1 {
+        return false
+    }
+    sum := 0
+    for i:=1; i*i <= num; i++ {
+        if num % i == 0 {
+            sum += i + (num / i)
+        }
+    }
+    fmt.Println(sum)
+    if sum-num == num {
+        return true
+    }
+    return false
+}
