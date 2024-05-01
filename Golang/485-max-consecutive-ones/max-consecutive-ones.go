f4453314e372464495a5937f5a69595a781b2aca
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-01 21:36:36 +0530

    Time: 32 ms (35.71%) | Memory: 6.8 MB (66.45%) - LeetSync

--- Golang/485-max-consecutive-ones/max-consecutive-ones.go
+++ Golang/485-max-consecutive-ones/max-consecutive-ones.go
@@ -1,17 +1,10 @@
 func findMaxConsecutiveOnes(nums []int) int {
     count, maxCount := 0, 0
     for i := 0; i < len(nums); i++ {
-        if nums[i] == 1 {
-            count += 1
-        } else {
-            if count > maxCount {
-                maxCount = count
-            }
-            count = 0
+        count = count * nums[i] + nums[i]
+        if count > maxCount {
+            maxCount = count
         }
-    }
-    if count > maxCount {
-        maxCount = count
     }
     return maxCount
 }
