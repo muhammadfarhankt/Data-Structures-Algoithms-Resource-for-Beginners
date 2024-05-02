c1e4700d583efedf30e100f9a8bc9e3d86457a0d
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-02 20:59:22 +0530

    Time: 201 ms (40.30%) | Memory: 12.6 MB (88.81%) - LeetSync

--- Golang/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.go
+++ Golang/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.go
@@ -8,8 +8,6 @@ func rearrangeArray(nums []int) []int {
             positive = append(positive, num)
         }
     }
-    fmt.Println(positive)
-    fmt.Println(negative)
     for odd, even, i := 0, 0, 0; i < size; i++ {
         if i % 2 == 0 {
             nums[i] = positive[even]
