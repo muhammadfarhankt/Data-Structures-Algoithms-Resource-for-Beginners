13a4568cde6c1c198f5284538734256d83ebcbe2
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-02 21:02:54 +0530

    Time: 194 ms (52.98%) | Memory: 16.8 MB (29.10%) - LeetSync

--- Golang/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.go
+++ Golang/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.go
@@ -1,21 +1,14 @@
 func rearrangeArray(nums []int) []int {
     size := len(nums)
-    positive, negative := make([]int, 0, size/2), make([]int, 0, size/2)
-    for _, num := range nums {
-        if num < 0 {
-            negative = append(negative, num)
+    result := make([]int, size, size)
+    for i, pos, neg := 0, 0, 1; i < size; i++ {
+        if nums[i] < 0 {
+            result[neg] = nums[i]
+            neg += 2
         } else {
-            positive = append(positive, num)
+            result[pos] = nums[i]
+            pos += 2
         }
     }
-    for odd, even, i := 0, 0, 0; i < size; i++ {
-        if i % 2 == 0 {
-            nums[i] = positive[even]
-            even++
-        } else {
-            nums[i] = negative[odd]
-            odd++
-        }
-    }
-    return nums
+    return result
 }
