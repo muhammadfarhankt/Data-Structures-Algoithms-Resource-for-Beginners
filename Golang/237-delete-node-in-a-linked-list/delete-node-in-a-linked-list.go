7630abff5e0fe14ad32be965b0bbc0fa2b137167
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-07 21:36:57 +0530

    Time: 3 ms (71.86%) | Memory: 3 MB (68.84%) - LeetSync

---
+++ Golang/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.go
@@ -0,0 +1,10 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+    *node = *node.Next
+}
