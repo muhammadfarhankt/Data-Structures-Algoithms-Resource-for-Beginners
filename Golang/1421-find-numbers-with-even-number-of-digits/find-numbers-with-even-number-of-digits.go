c794a71aa30bf961f1abf5901e3f0d1eb00a00c6
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-08 21:25:52 +0530

    Time: 4 ms (58.77%) | Memory: 3 MB (61.04%) - LeetSync

--- Golang/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.go
+++ Golang/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.go
@@ -1,11 +1,7 @@
 func findNumbers(nums []int) int {
     evenDigits := 0
     for _, num := range nums {
-        if num > 9 && num < 100 {
-            evenDigits++
-        } else if num > 999 && num < 10000 {
-            evenDigits++
-        } else if num == 100000 {
+        if num > 9 && num < 100 || num > 999 && num < 10000 || num == 100000 {
             evenDigits++
         }
     }
