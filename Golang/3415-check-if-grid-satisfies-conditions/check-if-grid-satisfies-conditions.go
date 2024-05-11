0b693201b1bfd09ff951cd5ec6b8dee0b33696df
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-12 00:01:09 +0530

    Time: 5 ms (100.00%) | Memory: 3.7 MB (100.00%) - LeetSync

--- Golang/3415-check-if-grid-satisfies-conditions/check-if-grid-satisfies-conditions.go
+++ Golang/3415-check-if-grid-satisfies-conditions/check-if-grid-satisfies-conditions.go
@@ -1,20 +1,21 @@
 func satisfiesConditions(grid [][]int) bool {
-  m, n := len(grid), len(grid[0])
-
-  for i := 0; i < m-1; i++ { // Check up to m-1 rows (exclude last row in main loop)
-    for j := 0; j < n; j++ {
-      if grid[i][j] != grid[i + 1][j] && (i + 1 == m || grid[i][j] != grid[i + 1][j]) {
-        return false
-      }
+    column, row := len(grid[0]), len(grid)
+    for i := 0; i < row-1; i++ {
+        for j := 0; j < column; j++ {
+            if grid[i][j] != grid[i+1][j] {
+                return false
+            }
+            if j < column-1 && grid[i][j] == grid[i][j+1] {
+                return false
+            }
+        }
     }
-  }
 
-  // Check last row separately (always)
-  for j := 0; j < n-1; j++ { // Check up to n-1 (exclude last column)
-    if grid[m-1][j] == grid[m-1][j+1] {
-      return false
+    // Check last row separately (always)
+    for j := 0; j < column-1; j++ {
+        if grid[row-1][j] == grid[row-1][j+1] {
+            return false
+        }
     }
-  }
-
-  return true
+    return true
 }
