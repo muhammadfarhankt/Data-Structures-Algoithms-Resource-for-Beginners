f9d9c54eaec4ef2da8bdcfe9c53497c2a9a14511
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-13 09:16:22 +0530

    Time: 204 ms (100.00%) | Memory: 10.8 MB (100.00%) - LeetSync

--- Golang/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.go
+++ Golang/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.go
@@ -1,19 +1,16 @@
 func maximumEnergy(energy []int, k int) int {
     maxEnergy := -1001
     size := len(energy)
-
     for i := k; i < size; i++ {
         val := energy[i] + energy[i-k]
         if val > energy[i] {
             energy[i] = val
         }
     }
-
     for i := size - 1; i >= size - k; i-- {
         if energy[i] > maxEnergy {
             maxEnergy = energy[i]
         }
     }
-    
     return maxEnergy
 }
