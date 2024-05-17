63057ff543dab9b3051bffc7ef531fbb88076b3b
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-17 10:28:38 +0530

    Time: 9 ms (49.57%) | Memory: 6.7 MB (75.21%) - LeetSync

--- Golang/2032-largest-odd-number-in-string/largest-odd-number-in-string.go
+++ Golang/2032-largest-odd-number-in-string/largest-odd-number-in-string.go
@@ -1,8 +1,7 @@
 func largestOddNumber(num string) string {
-    size := len(num)
-    for i := size - 1; i >= 0; i-- {
+    for i := len(num) - 1; i >= 0; i-- {
         if int(num[i]) % 2 == 1 {
-            fmt.Println(num[:i])
+            // fmt.Println(num[:i])
             return num[:i+1]
         }
     }
