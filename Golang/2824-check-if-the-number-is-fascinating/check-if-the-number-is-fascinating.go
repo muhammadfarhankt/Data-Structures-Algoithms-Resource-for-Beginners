a98e7fe29fcf7333120bdb3040284949ae9b44dc
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-28 10:01:23 +0530

    Time: 5 ms (31.25%) | Memory: 2.3 MB (100.00%) - LeetSync

---
+++ Golang/2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.go
@@ -0,0 +1,22 @@
+func isFascinating(n int) bool {
+    num := make([]int, 10)
+    for temp := n; temp > 0; temp /= 10 {
+        num[(temp%10)]++
+    }
+    for temp := 2*n; temp > 0; temp /= 10 {
+        num[(temp%10)]++
+    }
+    for temp := 3*n; temp > 0; temp /= 10 {
+        num[(temp%10)]++
+    }
+    // fmt.Println(num)
+    if num[0] > 0 {
+        return false
+    }
+    for i := 1; i < 10; i++ {
+        if num[i] > 1 {
+            return false
+        }
+    }
+    return true
+}
