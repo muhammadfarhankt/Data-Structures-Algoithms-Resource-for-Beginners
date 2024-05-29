019480126fd3fee10abc7f3e4a9cede7815d4b6e
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-05-30 00:11:32 +0530

    Time: 14 ms (95.28%) | Memory: 7.6 MB (14.17%) - LeetSync

--- Golang/2502-sort-the-people/sort-the-people.go
+++ Golang/2502-sort-the-people/sort-the-people.go
@@ -1,13 +1,11 @@
 func sortPeople(names []string, heights []int) []string {
     n := len(names)
-    heightsMap := make(map[int]string, n)
+    heightsMap := make(map[int]string)
     for idx, height := range heights {
         heightsMap[height] = names[idx]
     }
     sort.Ints(heights)
     output := make([]string, 0, n)
-    // fmt.Println("heights : ", heights)
-    // fmt.Println(heightsMap)
     for i := n-1; i >= 0; i-- {
         output = append(output, heightsMap[heights[i]])
     }
