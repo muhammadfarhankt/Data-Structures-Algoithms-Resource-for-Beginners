869b74e8c4eee651214a9beadeb3e19574e63a1e
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-07 15:36:46 +0530

    Time: 7 ms (86.58%) | Memory: 6.4 MB (48.42%) - LeetSync

--- Golang/2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.go
+++ Golang/2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.go
@@ -2,11 +2,9 @@ func largestLocal(grid [][]int) [][]int {
     n := len(grid)
     // fmt.Println(n)
     output := make([][]int, (n-2), (n-2))
-    for i := 0; i < n-2; i++ {
-        output[i] = make([]int, n-2)
-    }
     // fmt.Println(output)
     for i := 0; i < n-2; i++ {
+        output[i] = make([]int, n-2)
         for j := 0; j < n-2; j++ {
             max := grid[i][j]
             for k := i; k < i+3; k++ {
