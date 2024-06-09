4909caf09c49cb90255f9e20621ce35c8b36cdb9
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-09 23:08:44 +0530

    Time: 101 ms (71.13%) | Memory: 9.7 MB (51.55%) - LeetSync

--- Golang/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.go
+++ Golang/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.go
@@ -2,7 +2,9 @@ func construct2DArray(original []int, m int, n int) [][]int {
     if n * m != len(original) {
         return [][]int{}
     }
+
     output := make([][]int, m)
+    
     for i, idx := 0, 0; i < m; i++ {
         output[i] = make([]int, n)
         for j := 0; j < n; j++ {
@@ -10,5 +12,6 @@ func construct2DArray(original []int, m int, n int) [][]int {
             idx++
         }
     }
+    
     return output
 }
