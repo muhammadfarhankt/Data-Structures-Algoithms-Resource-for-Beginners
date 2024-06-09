64968d07194bda8bd839ec923e055e453bd4baf4
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-09 22:59:28 +0530

    Time: 4 ms (100.00%) | Memory: 2.4 MB (77.59%) - LeetSync

--- Golang/1656-count-good-triplets/count-good-triplets.go
+++ Golang/1656-count-good-triplets/count-good-triplets.go
@@ -1,8 +1,8 @@
 func countGoodTriplets(arr []int, a int, b int, c int) int {
-    count := 0
-    for i := 0; i < len(arr); i++ {
-        for j := i+1; j < len(arr); j++ {
-            for k := j + 1; k < len(arr); k++ {
+    size, count := len(arr), 0
+    for i := 0; i < size; i++ {
+        for j := i+1; j < size; j++ {
+            for k := j + 1; k < size; k++ {
                 if abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c {
                     count++
                 }
