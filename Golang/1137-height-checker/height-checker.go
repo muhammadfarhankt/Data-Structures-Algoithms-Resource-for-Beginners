0f96e09629f670431da3b9932a0996c57292ad67
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-10 09:34:11 +0530

    Time: 2 ms (65.22%) | Memory: 2.3 MB (84.24%) - LeetSync

--- Golang/1137-height-checker/height-checker.go
+++ Golang/1137-height-checker/height-checker.go
@@ -2,7 +2,7 @@ func heightChecker(heights []int) int {
     temp := make([]int, 0, len(heights)) 
     temp = append(temp, heights...)
     count := 0
-    sort.Ints(temp)
+    slices.Sort(temp)
     for i := 0; i < len(heights); i++ {
         if heights[i] != temp[i] {
             count++
