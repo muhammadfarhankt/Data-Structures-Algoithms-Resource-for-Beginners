5ca5cf63cdfd6771376da67350d09f3b46c39c4f
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-10 10:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 2.5 MB (100.00%) - LeetSync

--- Golang/2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.go
+++ Golang/2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.go
@@ -1,7 +1,15 @@
 func countElements(nums []int) int {
-    sort.Ints(nums)
-    min, max := nums[0], nums[len(nums)-1]
+    max, min := -100001, 100001
     count := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] > max {
+            max = nums[i]
+        }
+        if nums[i] < min {
+            min = nums[i]
+        }
+    }
+    fmt.Println("max : ", max, "\tmin : ", min)
     for i := 0; i < len(nums); i++ {
         if nums[i] > min && nums[i] < max{
             count++
