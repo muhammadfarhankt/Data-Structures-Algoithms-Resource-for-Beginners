38e423a72634c174427b7ed8a98d8775a16c4a08
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-11 10:05:14 +0530

    Time: 1 ms (82.05%) | Memory: 2.4 MB (100.00%) - LeetSync

---
+++ Golang/1217-relative-sort-array/relative-sort-array.go
@@ -0,0 +1,25 @@
+func relativeSortArray(arr1 []int, arr2 []int) []int {
+    idxArr1, idxArr2 := make([]int, 1001), make([]bool, 1001)
+    size1, size2 := len(arr1), len(arr2)
+    output := make([]int, 0, (size1 + size2))
+    for _, num := range arr1 {
+        idxArr1[num]++
+    }
+    for _, num := range arr2 {
+        idxArr2[num] = true
+    }
+    for _, num := range arr2 {
+        output = append(output, num)
+        for idxArr1[num] > 1 {
+            output = append(output, num)
+            idxArr1[num]--
+        }
+        idxArr1[num]--
+    }
+    for idx, num := range idxArr1 {
+        for i := 1; i <= num; i++ {
+            output = append(output, idx)
+        }
+    }
+    return output
+}
