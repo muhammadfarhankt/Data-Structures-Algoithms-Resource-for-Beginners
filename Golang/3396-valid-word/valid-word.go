3b74fff0f4a54e3d27e58d91b8d42e844bd37bd4
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-15 15:02:36 +0530

    Time: 4 ms (21.46%) | Memory: 2.4 MB (7.80%) - LeetSync

--- Golang/3396-valid-word/valid-word.go
+++ Golang/3396-valid-word/valid-word.go
@@ -2,14 +2,14 @@ func isValid(word string) bool {
     if len(word) < 3 {
         return false
     }
-    vowelMap := map[rune]bool{'a': true, 'e': true, 'i': true, 'o': true, 'u': true, 'A': true, 'E': true, 'I': true, 'O': true, 'U': true}
+    vowelMap := map[byte]bool{'a': true, 'e': true, 'i': true, 'o': true, 'u': true, 'A': true, 'E': true, 'I': true, 'O': true, 'U': true}
     vowel, consonant := false, false
-    for _, char := range word {
-        if vowelMap[char] {
+    for i := 0; i < len(word); i++ {
+        if vowelMap[word[i]] {
             vowel = true
-        } else if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' {
+        } else if word[i] >= 'a' && word[i] <= 'z' || word[i] >= 'A' && word[i] <= 'Z' {
             consonant = true
-        } else if char >= '0' && char <= '9' {
+        } else if word[i] >= '0' && word[i] <= '9' {
             continue
         } else {
             return false
