aa785d40e3c94f34b92dcc0484ff5c8da97d3b07
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-06-29 22:59:12 +0530

    Time: 1 ms (58.33%) | Memory: 2.2 MB (91.67%) - LeetSync

---
+++ Golang/2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.go
@@ -0,0 +1,17 @@
+func isSumEqual(firstWord string, secondWord string, targetWord string) bool {
+    word1, word2, targetSum := 0, 0, 0
+    for _, char := range firstWord {
+        word1 = word1 * 10 + int(char - 'a')
+    }
+    for _, char := range secondWord {
+        word2 = word2 * 10 + int(char - 'a')
+    }
+    for _, char := range targetWord {
+        targetSum = targetSum * 10 + int(char - 'a')
+    }
+    fmt.Println(word2, targetSum)
+    if word1 + word2 == targetSum {
+        return true
+    }
+    return false
+}
