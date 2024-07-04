1c024f912a5192ea258bb5f5fd993f5e1f75b2af
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-07-04 23:27:28 +0530

    Time: 1 ms (74.50%) | Memory: 2.3 MB (66.59%) - LeetSync

---
+++ Golang/100-same-tree/same-tree.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
+        return true
+    } else if p == nil || q == nil {
+        return false
+    }
+    if p.Val != q.Val {
+        return false
+    }
+    return ( isSameTree(p.Left,q.Left) && isSameTree(p.Right,q.Right) )
+    return true
+}
