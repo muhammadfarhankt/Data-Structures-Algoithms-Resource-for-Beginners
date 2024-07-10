4900730cb1eeb50a839806a157b190a0faab23a5
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-07-10 21:53:00 +0530

    Time: 2 ms (11.95%) | Memory: 2.8 MB (22.01%) - LeetSync

---
+++ Golang/118-pascals-triangle/pascals-triangle.go
@@ -0,0 +1,16 @@
+func generate(numRows int) [][]int {
+    output := make([][]int, 0, numRows)
+    output = append(output, []int{1})
+    fmt.Println(output)
+    for i := 1; i < numRows; i++ {
+        newRow := make([]int, 0, i+1)
+        newRow = append(newRow, 1)
+        for j := 1; j < i; j++ {
+            newRow = append(newRow, output[i-1][j-1]+output[i-1][j])
+        }
+        newRow = append(newRow, 1)
+        output = append(output, newRow)
+    }
+    fmt.Println(output)
+    return output
+}
