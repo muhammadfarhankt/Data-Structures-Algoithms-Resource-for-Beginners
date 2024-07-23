e55e35ca34ae3a0c5dd440fd85735eb6def79818
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-07-23 13:52:49 +0530

    Time: 263 ms (46.14%) | Memory: 0B (100.00%) - LeetSync

--- Golang/1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ Golang/1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,5 +1,5 @@
 SELECT p.product_name, SUM(o.unit) AS unit
 FROM products AS p JOIN orders AS o ON p.product_id = o.product_id
 WHERE o.order_date >= '2020-02-01' AND o.order_date <= '2020-02-29'
-GROUP BY o.product_id, p.product_name
+GROUP BY p.product_name
 HAVING SUM(o.unit) > 99;
