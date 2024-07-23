e3bde539a05676f4918db000d9aa48fc2dca35a0
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-07-23 12:27:47 +0530

    Time: 85 ms (76.70%) | Memory: 7.6 MB (79.13%) - LeetSync

--- Golang/1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.go
+++ Golang/1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.go
@@ -1,10 +1,10 @@
 func replaceElements(arr []int) []int {
     for i, max := len(arr)-1, -1; i >= 0; i-- {
-        temp := max
         if arr[i] > max {
-            max = arr[i]
+            arr[i], max = max, arr[i]
+        } else {
+            arr[i] = max
         }
-        arr[i] = temp
     }
     return arr
 }
