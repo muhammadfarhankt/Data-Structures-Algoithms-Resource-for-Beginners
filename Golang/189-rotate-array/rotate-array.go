cfa73e914bcddbccf7e02447594634e5d1cb4299
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-07-29 12:13:48 +0530

    Time: 23 ms (72.91%) | Memory: 10.8 MB (5.51%) - LeetSync

--- Golang/189-rotate-array/rotate-array.go
+++ Golang/189-rotate-array/rotate-array.go
@@ -4,14 +4,14 @@ func rotate(nums []int, k int)  {
     for i := 0; i < size / 2; i++ {
         nums[i], nums[size-i-1] = nums[size-i-1], nums[i]
     }
-    fmt.Println("Reverse : ", nums)
+    // fmt.Println("Reverse : ", nums)
     for i := 0; i < k/2; i++ {
         nums[i], nums[k-i-1] = nums[k-i-1], nums[i]
     }
-    fmt.Println("first : ", nums)
+    // fmt.Println("first : ", nums)
     for i, j := k, 1; i < (size + k) / 2; i++ {
         nums[i], nums[size-j] = nums[size-j], nums[i]
         j++
     }
-    fmt.Println("second : ", nums)
+    // fmt.Println("second : ", nums)
 }
