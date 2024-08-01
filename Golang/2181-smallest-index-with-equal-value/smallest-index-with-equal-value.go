b059285dce52e99c44504fb0da006bbe6d00dab7
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-08-01 11:20:12 +0530

    Time: 5 ms (65.38%) | Memory: 3.7 MB (69.23%) - LeetSync

---
+++ Golang/2181-smallest-index-with-equal-value/smallest-index-with-equal-value.go
@@ -0,0 +1,13 @@
+func smallestEqual(nums []int) int {
+    mod := 101
+    for i, num := range nums {
+        if i % 10 == num && i < mod {
+            fmt.Println(mod, i, num)
+            mod = i
+        }
+    }
+    if mod == 101 {
+        return -1
+    }
+    return mod
+}
