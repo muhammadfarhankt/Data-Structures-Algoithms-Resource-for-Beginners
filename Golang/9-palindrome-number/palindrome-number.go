b33d63541ab0f39ce3178e8a49b25983822c4d75
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-08-05 22:40:48 +0530

    Time: 9 ms (70.01%) | Memory: 4.3 MB (50.81%) - LeetSync

--- Golang/9-palindrome-number/palindrome-number.go
+++ Golang/9-palindrome-number/palindrome-number.go
@@ -1,22 +1,18 @@
 func isPalindrome(x int) bool {
-    
     if x < 0 {
         return false
     }
     if x < 10 {
         return true
     }
-
     temp := 0
     newNum := 0
     y := x
-    
     for y > 0 {
         temp = y%10
         newNum = newNum*10 + temp
         y=y/10
     }
-    
     if newNum == x {
         return true
     }
