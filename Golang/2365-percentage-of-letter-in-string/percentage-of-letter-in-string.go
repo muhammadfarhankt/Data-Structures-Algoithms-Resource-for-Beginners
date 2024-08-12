1efbd24d9cfbefe081641629f9acb1ecbb954a86
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-08-12 21:45:07 +0530

    Time: 1 ms (61.11%) | Memory: 2.1 MB (87.04%) - LeetSync

--- Golang/2365-percentage-of-letter-in-string/percentage-of-letter-in-string.go
+++ Golang/2365-percentage-of-letter-in-string/percentage-of-letter-in-string.go
@@ -1,9 +1,10 @@
 func percentageLetter(s string, letter byte) int {
-    count := 0
+    count := 0;
     for i := 0; i < len(s); i++ {
-        if letter == s[i] {
+        if s[i] == letter {
             count++
         }
     }
-    return int((float64(count)/float64(len(s)))*100)
+    // fmt.Println("COUNT : ", count, "LENGTH  : ", len(s))
+    return int(float64(count) / float64(len(s)) * 100)
 }
