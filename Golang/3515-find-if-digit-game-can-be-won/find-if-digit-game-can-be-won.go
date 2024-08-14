e29c97b9631488c380c2631a9e8023dc31b832db
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-08-14 21:08:44 +0530

    Time: 4 ms (40.75%) | Memory: 3.2 MB (5.66%) - LeetSync

---
+++ Golang/3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.go
@@ -0,0 +1,11 @@
+func canAliceWin(nums []int) bool {
+    singleSum, doubleSum := 0, 0
+    for _, num := range nums {
+        if num < 10 {
+            singleSum += num
+        } else {
+            doubleSum += num
+        }
+    }
+    return singleSum > doubleSum || doubleSum > singleSum
+}
