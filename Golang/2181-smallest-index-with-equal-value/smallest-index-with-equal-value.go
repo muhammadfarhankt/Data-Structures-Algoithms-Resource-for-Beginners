248e577d2135a083ee00a212daf7eeb3b21c9dc5
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-08-19 22:20:58 +0530

    Time: 9 ms (42.42%) | Memory: 3.7 MB (69.70%) - LeetSync

--- Golang/2181-smallest-index-with-equal-value/smallest-index-with-equal-value.go
+++ Golang/2181-smallest-index-with-equal-value/smallest-index-with-equal-value.go
@@ -1,13 +1,9 @@
 func smallestEqual(nums []int) int {
-    mod := 101
-    for i, num := range nums {
-        if i % 10 == num && i < mod {
-            // fmt.Println(mod, i, num)
-            mod = i
+    for i, v := range nums {
+        if i % 10 == v {
+            return i
         }
     }
-    if mod == 101 {
-        return -1
-    }
-    return mod
+    
+    return -1
 }
