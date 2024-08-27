5ff1d87017be936f6f55fb96dc1e055d0f0bff41
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2024-08-27 15:39:33 +0530

    Time: 2 ms (64.34%) | Memory: 2.8 MB (43.08%) - LeetSync

--- Golang/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
+++ Golang/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
@@ -1,44 +1,17 @@
     func gcdOfStrings(str1 string, str2 string) string {
-        var small string
-        var large string
-        sSize, lSize := 0, 0
-        if len(str1) > len(str2) {
-            small = str2
-            large = str1
-            sSize, lSize = len(str2), len(str1)
-        } else {
-            small = str1
-            large = str2
-            sSize, lSize = len(str1), len(str2)
+        if str1 + str2 != str2 + str1 {
+            return ""
         }
-        // fmt.Println("Small : ", small)
-        // fmt.Println("size : ", sSize, "\nLarge : ", large, "Size : ", lSize)
-        for i := sSize; i > 0; i-- {
-            if sSize % i == 0 && lSize % i == 0 {
-                // fmt.Println(small[:i])
-                temp1 := strings.Repeat(small[:i], sSize / i)
-                temp2 := strings.Repeat(small[:i], lSize / i)
-                if temp1 == small && temp2 == large {
-                    return small[:i]
-                }
-            }
+        if str1 == str2 {
+            return str1
         }
-        // for i := 0; i < len(small); i++ {
-        //     fmt.Println("small[:(i+1)] : ", small[0:i+1])
-        //     temp := strings.Repeat(small[0:i+1], len(small) / (i+1))
-        //     if temp == small {
-        //         fmt.Println("small same")
-        //         for j := i; j < len(large); j++ {
-        //             fmt.Println("large ; ", strings.Repeat(small[0:i+1], j))
-        //             fmt.Println("large og : ", large)
-        //             if strings.Repeat(small[0:i+1], j) == large {
-        //                 fmt.Println("same")
-        //                 // fmt.Println("temp : ", temp)
-        //                 return small[0:i+1]
-        //             }
-        //         }
+        val := gcd(len(str1), len(str2))
+        return str1[:val]
+    }
+    func gcd(a, b int) int {
+        for b != 0 {
+            a, b = b, a % b
+        }
+        return a
 
-        //     }
-        // }
-        return ""
     }
