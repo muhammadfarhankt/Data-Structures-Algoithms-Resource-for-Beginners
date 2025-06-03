56407d9482cad18e1923c866141279577fc2e455
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2025-06-03 23:02:17 +0400

    Time: 0 ms (100.00%) | Memory: 5.4 MB (60.98%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.go
+++ 412-fizz-buzz/fizz-buzz.go
@@ -1,14 +1,14 @@
 func fizzBuzz(n int) []string {
-    output := make([]string, 0, n)
+    output := make([]string, n)
     for i := 1; i <= n; i++ {
         if i % 3 == 0 && i % 5 == 0 {
-            output = append(output, "FizzBuzz")
+            output[i-1] = "FizzBuzz"
         } else if i % 3 == 0 {
-            output = append(output, "Fizz")
+            output[i-1] = "Fizz"
         } else if i % 5 == 0 {
-            output = append(output, "Buzz")
+            output[i-1] = "Buzz"
         } else {
-            output = append(output, strconv.Itoa(i))
+            output[i-1] = strconv.Itoa(i)
         }
     }
     return output
