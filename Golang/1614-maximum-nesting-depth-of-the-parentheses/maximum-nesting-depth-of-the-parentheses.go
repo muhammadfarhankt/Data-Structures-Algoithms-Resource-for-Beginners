6f6ea4c2885d6f96d59a95a0c30392c91a85acd3
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2025-06-05 14:29:24 +0400

    Time: 0 ms (100.00%) | Memory: 3.9 MB (41.67%) - LeetSync

--- Golang/1614-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.go
+++ Golang/1614-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.go
@@ -2,15 +2,11 @@ func maxDepth(s string) int {
     max, depth := 0, 0
     for _, ch := range s {
         if ch == '(' {
-            fmt.Println("(")
             depth++
-            fmt.Println("depth : ", depth)
             if depth > max {
                 max = depth
-                fmt.Println("max : ", max)
             }
         } else if ch == ')' {
-            fmt.Println(")")
             depth--
         }
     }
