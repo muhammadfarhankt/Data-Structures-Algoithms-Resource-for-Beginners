50e03209c8f79b9d181ef03a467035792a04ef40
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2025-09-16 23:38:59 +0400

    Time: 0 ms (100.00%) | Memory: 4 MB (52.00%) - LeetSync

--- Golang/3663-find-the-least-frequent-digit/find-the-least-frequent-digit.go
+++ Golang/3663-find-the-least-frequent-digit/find-the-least-frequent-digit.go
@@ -5,7 +5,7 @@ func getLeastFrequentDigit(n int) int {
         n = n / 10
         arr[temp]++
     }
-    fmt.Println(arr)
+    // fmt.Println(arr)
     for i := 1; i <= 9; i++ {
         for j := 0; j <= 9; j++ {
             if arr[j] == i {
