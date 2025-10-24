449ea9c2b449df67a17792d47cd651befe855b92
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2025-10-24 19:01:13 +0400

    Time: 3 ms (56.25%) | Memory: 7.4 MB (76.56%) - LeetSync

--- Golang/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.go
+++ Golang/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.go
@@ -1,18 +1,19 @@
 func findMissingAndRepeatedValues(grid [][]int) []int {
     n := len(grid)
     a, b := 0, 0
-    gridMap := make(map[int]bool, n*n)
+    gridArr := make([]bool, n*n)
     for i := 0; i < n; i++ {
         for j := 0; j < n; j++ {
-            if gridMap[grid[i][j]] == true {
+            if gridArr[grid[i][j]-1] == true {
                 a = grid[i][j]
             } else {
-                gridMap[grid[i][j]] = true
+                gridArr[grid[i][j]-1] = true
             }
         }
     }
+    fmt.Println(gridArr)
     for i := 1; i <= n*n; i++ {
-        if gridMap[i] != true {
+        if gridArr[i-1] != true {
             b = i
             break
         }
