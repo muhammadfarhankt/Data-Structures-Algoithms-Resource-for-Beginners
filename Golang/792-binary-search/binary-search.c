a15299d149c9ccddb14361c5578aeab6e8f835e8
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2025-12-12 22:56:43 +0400

    Time: 0 ms (100.00%) | Memory: 9.2 MB (97.68%) - LeetSync

---
+++ Golang/792-binary-search/binary-search.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target) {
+    int start, end, mid;
+    start = 0;
+    end = numsSize - 1;
+    while (start <= end) {
+        mid = (start + end) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
