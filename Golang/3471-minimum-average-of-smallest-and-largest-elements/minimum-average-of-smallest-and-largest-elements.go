04e76261272657245f875128e684316fb2c24958
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2026-01-07 23:55:36 +0400

    Time: 0 ms (100.00%) | Memory: 4.5 MB (40.63%) - LeetSync

--- Golang/3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.go
+++ Golang/3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.go
@@ -2,8 +2,6 @@ func minimumAverage(nums []int) float64 {
     n := len(nums)
     var minAvg float64
     sort.Ints(nums)
-    //fmt.Println(nums)
-    //fmt.Println(avgArr)
     minAvg = float64(nums[0] + nums[n-1]) / float64(2)
     for i := 1; i < (n/2); i++ {
         avg := float64(nums[i] + nums[n-1-i]) / float64(2)
