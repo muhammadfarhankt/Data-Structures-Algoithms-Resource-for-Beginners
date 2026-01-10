1377aefa537acd83ba96885cbc39c902f51445d8
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2026-01-10 18:55:34 +0400

    Time: 332 ms (54.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ Golang/1523-capital-gainloss/capital-gainloss.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT stock_name, SUM(
+    CASE
+    WHEN operation = 'Buy' THEN -price 
+    ELSE price
+    END
+) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
