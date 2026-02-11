5e8914845c397878aa816e8872cf2c85e7d0bc07
Muhammad Farhan KT <50117098+muhammadfarhankt@users.noreply.github.com>
2026-02-11 21:37:41 +0400

    Time: 64 ms (1.99%) | Memory: 5.7 MB (10.60%) - LeetSync

---
+++ Golang/4214-count-dominant-indices/count-dominant-indices.go
@@ -0,0 +1,17 @@
+func dominantIndices(nums []int) int {
+    count, length := 0, len(nums)
+    avgArr := make([]int, length, length)
+    for i, sum := length - 2, 0; i >= 0; i-- {
+        sum += nums[i+1]
+        avgArr[i] = sum / (length - i - 1)
+    }
+    for i := 0; i < length - 1; i++ {
+        fmt.Println(nums[i], avgArr[i])
+        if nums[i] > avgArr[i] {
+            count++
+        }
+        fmt.Println(count)
+    }
+    fmt.Println(avgArr)
+    return count
+}
